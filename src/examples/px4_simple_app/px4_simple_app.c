483f32360b407fe4cdf08e97eeb13a81bc922528
altlnt <letalenet@isir.upmc.fr>
2018-05-18 15:47:51 +0200

    Writing my first application

--- src/examples/px4_simple_app/px4_simple_app.c
+++ src/examples/px4_simple_app/px4_simple_app.c
@@ -55,7 +55,7 @@ __EXPORT int px4_simple_app_main(int argc, char *argv[]);
 
 int px4_simple_app_main(int argc, char *argv[])
 {
-	PX4_INFO("Hello Sky!");
+	PX4_INFO("Hello Drone 2018!");
 
 	/* subscribe to sensor_combined topic */
 	int sensor_sub_fd = orb_subscribe(ORB_ID(sensor_combined));
@@ -77,7 +77,7 @@ int px4_simple_app_main(int argc, char *argv[])
 
 	int error_counter = 0;
 
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 100; i++) {
 		/* wait for sensor update of 1 file descriptor for 1000 ms (1 second) */
 		int poll_ret = px4_poll(fds, 1, 1000);
 
